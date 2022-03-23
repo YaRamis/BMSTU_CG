8df6c95a99a8bb0cfb12c59868cea027280072fa
Myrzabekov Ramis <myyrzabekovrm@student.bmstu.ru>
2022-03-23 07:24:50 +0300

    variant task part done

--- ЛР 01/app.py
+++ ЛР 01/app.py
@@ -8,6 +8,76 @@ y_axis_center = 0
 
 k_scale = 1.0
 
+def get_distance_point_to_line(xp, yp, xa, ya, xb, yb):
+    return math.sqrt(((xp - xa) * (yb - ya) - (yp - ya) * (xb - xa))**2) / math.sqrt((xb - xa)**2 + (yb - ya)**2)
+
+def get_distance_point_to_point(xa, ya, xb, yb):
+    return math.sqrt((xb - xa)**2 + (yb - ya)**2)
+
+def get_point_location_from_line(xa, ya, xb, yb, xp, yp):
+    A = ya - yb
+    B = xb - xa
+    C = xa * yb - xb * ya
+    return A * xp + B * yp + C
+
+def do_task(dots_dict, circle_info):
+    x_circle, y_circle, r = circle_info
+    dots = []
+    dots_in_circle = []
+
+    for dot_id in dots_dict:
+        x_dot, y_dot = dots_dict[dot_id]
+        dots.append((dot_id, x_dot, y_dot))
+        if get_distance_point_to_point(x_dot, y_dot, x_circle, y_circle) <= r:
+            dots_in_circle.append((dot_id, x_dot, y_dot))
+    print(dots_in_circle)
+    dots_l = []
+    dots_r = []
+    id_a = -1
+    id_b = -1
+    # flag = 0
+    for i in range(len(dots) - 1):
+        # if flag == 0:
+        xa, ya = dots[i][1], dots[i][2]
+        id_a_temp = dots[i][0]
+        for j in range(i + 1, len(dots)):
+            dots_l_temp = []
+            dots_r_temp = []
+            xb, yb = dots[j][1], dots[j][2]
+            id_b_temp = dots[j][0]
+            h = get_distance_point_to_line(x_circle, y_circle, xa, ya, xb, yb)
+            if h < r:
+                for k in range(len(dots_in_circle)):
+                    if dots_in_circle[k][0] != id_a_temp and dots_in_circle[k][0] != id_b_temp:
+                        xp, yp = dots_in_circle[k][1], dots_in_circle[k][2]
+                        location = get_point_location_from_line(xa, ya, xb, yb, xp, yp)
+                        if ya < yb:
+                            if location > 0:
+                                dots_l_temp.append(dots_in_circle[k])
+                            elif location < 0:
+                                dots_r_temp.append(dots_in_circle[k])
+                        elif ya >= yb:
+                            if location < 0:
+                                dots_l_temp.append(dots_in_circle[k])
+                            elif location > 0:
+                                dots_r_temp.append(dots_in_circle[k])
+                if dots_l == [] and dots_r == []:
+                    dots_r = dots_r_temp
+                    dots_l = dots_l_temp
+                    id_a = id_a_temp
+                    id_b = id_b_temp
+                elif abs(len(dots_l_temp) - len(dots_r_temp)) < abs(len(dots_l) - len(dots_r)) and dots_l_temp != [] and dots_r_temp != []:
+                    dots_r = dots_r_temp
+                    dots_l = dots_l_temp
+                    id_a = id_a_temp
+                    id_b = id_b_temp
+
+    print(id_a, id_b)
+    print(dots_l)
+    print(dots_r)
+
+    return id_a, id_b, dots_l, dots_r
+
 class App():
     def __init__(self) -> None:
         self.root = Tk()
@@ -25,8 +95,21 @@ class App():
         self.canv = Canvas(self.root, highlightthickness=0, bg='white')
         self.canv.place(relwidth=0.7, relheight=1, relx=0.3)
 
+        self.menubar = Menu(self.root)
+        self.actionmenu = Menu(self.menubar, tearoff='off')
+        self.actionmenu.add_command(label='Запуск', command=self.prog_run)
+        self.actionmenu.add_command(label='Очистить', command=self.clear_all)
+        self.actionmenu.add_command(label='Выход', command=self.root.destroy)
+        self.menubar.add_cascade(label='Действия', menu=self.actionmenu)
+        self.infomenu = Menu(self.menubar, tearoff='off')
+        self.infomenu.add_command(label='О программе', command=self.prog_info)
+        self.infomenu.add_command(label='Об авторе', command=self.author_info)
+        self.menubar.add_cascade(label='Информация', menu=self.infomenu)
+        self.root.config(menu=self.menubar)
+
         self.dots_dict = dict([])
         self.circle_id = -1
+        self.circle_info = ()
 
         self.last_action = []
         self.root.bind_all("<Control-z>", self.redo)
@@ -152,6 +235,13 @@ class App():
         # ZOOM CANVAS
         self.canv.bind("<MouseWheel>",self.zoomer)
 
+        # RUN
+        self.lab_prog_run = Label(self.frame, font='"monotxt_iv50" 10', bg='#9c79d1', fg='#47385e', justify=CENTER, text='-------------------------------------ЗАПУСК-------------------------------------')
+        self.lab_prog_run.place(rely=0.91, relwidth=1)
+
+        self.btn_dot_edit = Button(self.frame, text='⯈', font='"monotxt_iv50" 18', bg='#ab85e6', activebackground='#b890f5', fg='#2b213b', command=self.prog_run)
+        self.btn_dot_edit.place(relwidth=0.9, relx=0.05, rely=0.94, relheight=0.04)
+
         self.root.mainloop()
     
     def entry_mode_x(self, event):
@@ -200,9 +290,15 @@ class App():
             self.ent_dot_edit_id['fg'] = '#2b213b'
     
     def create_dot(self, event):
+        x, y = (event.x - x_axis_center) / k_scale, -(event.y - y_axis_center) / k_scale
+        for i in self.dots_dict:
+            x_temp, y_temp = self.dots_dict[i]
+            if x_temp == x and y_temp == y:
+                return
         dot_id = self.canv.create_oval([event.x - 3, event.y - 3], [event.x + 3, event.y + 3], fill='#b890f5', tags='draggable')
-        self.dots_dict[dot_id] = (event.x, event.y)
-        self.dots_table.insert('', END, values=(dot_id, (event.x - x_axis_center) / k_scale, -(event.y - y_axis_center) / k_scale))
+        # self.dots_dict[dot_id] = (event.x, event.y)
+        self.dots_dict[dot_id] = (x, y)
+        self.dots_table.insert('', END, values=(dot_id, x, y))
         self.last_action = ['CreateDot', dot_id]
     
     def create_dot_btn(self):
@@ -222,8 +318,20 @@ class App():
             return
         x = float(x)
         y = float(y)
+        for i in self.dots_dict:
+            x_temp, y_temp = self.dots_dict[i]
+            if x_temp == x and y_temp == y:
+                messagebox.showerror(title='Ошибка!', message='Точка с такими координатами уже есть!')
+                self.ent_x.delete(0, END)
+                self.ent_y.delete(0, END)
+                self.ent_x.insert(0, 'X')
+                self.ent_y.insert(0, 'Y')
+                self.ent_x['fg'] = '#7b5ea6'
+                self.ent_y['fg'] = '#7b5ea6'
+                return
         dot_id = self.canv.create_oval([x * k_scale + x_axis_center - 3, (-y) * k_scale + y_axis_center - 3], [x * k_scale + x_axis_center + 3, (-y) * k_scale + y_axis_center + 3], fill='#b890f5', tags='draggable')
-        self.dots_dict[dot_id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+        # self.dots_dict[dot_id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+        self.dots_dict[dot_id] = (x, y)
         self.dots_table.insert('', END, values=(dot_id, x, y))
         self.ent_x.delete(0, END)
         self.ent_y.delete(0, END)
@@ -259,6 +367,7 @@ class App():
         y = float(y)
         r = float(r)
         self.circle_id = self.canv.create_oval([(x - r) * k_scale + x_axis_center, (-y - r) * k_scale + y_axis_center], [(x + r) * k_scale  + x_axis_center, (-y + r) * k_scale + y_axis_center])
+        self.circle_info = (x, y, r)
         self.canv.tag_lower(self.circle_id)
         self.canv.tag_lower(self.x_axis)
         self.canv.tag_lower(self.y_axis)
@@ -290,7 +399,6 @@ class App():
                 self.ent_dot_id.delete(0, END)
                 self.ent_dot_id.insert(0, 'ID')
                 self.ent_dot_id['fg'] = '#7b5ea6'
-                self.last_action = ['RemoveDot', self.dots_dict[id]]
                 self.dots_dict.pop(id)
                 for item in self.dots_table.get_children():
                     if id == self.dots_table.item(item)['values'][0]:
@@ -350,8 +458,8 @@ class App():
                 self.ent_dot_edit_id.delete(0, END)
                 self.ent_dot_edit_id.insert(0, 'ID')
                 self.ent_dot_edit_id['fg'] = '#7b5ea6'
-                self.last_action = ['EditDot', self.dots_dict[id]]
-                self.dots_dict[id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+                # self.dots_dict[id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+                self.dots_dict[id] = (x, y)
                 for item in self.dots_table.get_children():
                     if id == self.dots_table.item(item)['values'][0]:
                         self.last_action = ['EditDot', [id, self.dots_table.item(item)['values'][1], self.dots_table.item(item)['values'][2]]]
@@ -504,7 +612,7 @@ class App():
             elif action == 'RemoveDot':
                 x, y = float(self.last_action[1]), float(self.last_action[2])
                 dot_id = self.canv.create_oval([x * k_scale + x_axis_center - 3, (-y) * k_scale + y_axis_center - 3], [x * k_scale + x_axis_center + 3, (-y) * k_scale + y_axis_center + 3], fill='#b890f5', tags='draggable')
-                self.dots_dict[dot_id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+                self.dots_dict[dot_id] = (x, y)
                 self.dots_table.insert('', END, values=(dot_id, x, y))
             elif action == 'RemoveCircle':
                 x0, y0, x1, y1 = self.last_action[1]
@@ -514,7 +622,8 @@ class App():
                 x = float(x)
                 y = float(y)
                 self.canv.coords(dot_id, x * k_scale + x_axis_center - 3, (-y) * k_scale + y_axis_center - 3, x * k_scale + x_axis_center + 3, (-y) * k_scale + y_axis_center + 3)
-                self.dots_dict[dot_id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+                # self.dots_dict[dot_id] = (x * k_scale + x_axis_center, (-y) * k_scale + y_axis_center)
+                self.dots_dict[dot_id] = (x, y)
                 for item in self.dots_table.get_children():
                     if dot_id == self.dots_table.item(item)['values'][0]:
                         self.dots_table.item(item, values=(dot_id, x, y))
@@ -549,6 +658,77 @@ class App():
             self.canv.delete(self.selected_dot[0])
             self.canv.delete(self.selected_dot[1])
             self.selected_dot.clear()
+    
+    def prog_info(self):
+        messagebox.showinfo(title='О программе', message='Условие:\nНа плоскости заданы множество точек М и круг. Выбрать из М две'
+                                                        ' различные точки так, чтобы наименьшим образом различались количества точек в'
+                                                        ' круге, лежащие по разные стороны от прямой, проходящей через эти точки.'
+                                                        '\n\nПосле нажатия кнопки запуска программы, оси переместятся в центр окружности и'
+                                                        ' будет показан результат, а слева вместо изначального интерфейса будет'
+                                                        ' результат в письменном виде и кнопка для возврата.')
+        if len(self.dots_table.selection()) > 0:
+            self.dots_table.selection_remove(self.dots_table.selection()[0])
+            self.canv.delete(self.selected_dot[0])
+            self.canv.delete(self.selected_dot[1])
+            self.selected_dot.clear()
+        
+    def author_info(self):
+        messagebox.showinfo(title='Об авторе', message='Мырзабеков Рамис Майрамбекович\n'
+                                                        'Студент МГТУ им. Н. Э. Баумана\n'
+                                                        'Группа ИУ7-44Б')
+        if len(self.dots_table.selection()) > 0:
+            self.dots_table.selection_remove(self.dots_table.selection()[0])
+            self.canv.delete(self.selected_dot[0])
+            self.canv.delete(self.selected_dot[1])
+            self.selected_dot.clear()
+
+    def clear_all(self):
+        if self.circle_id != -1:
+            self.canv.delete(self.circle_id)
+            self.circle_id = -1
+        for id in self.dots_dict:
+            self.canv.delete(id)
+        self.dots_dict.clear()
+        for item in self.dots_table.get_children():
+            self.dots_table.delete(item)
+        if len(self.dots_table.selection()) > 0:
+            self.dots_table.selection_remove(self.dots_table.selection()[0])
+            self.canv.delete(self.selected_dot[0])
+            self.canv.delete(self.selected_dot[1])
+            self.selected_dot.clear()
+    
+    def prog_run(self):
+        if self.circle_id == -1:
+            messagebox.showerror(title='Ошибка!', message='Нету окружности!')
+            return
+        if len(self.dots_dict) < 2:
+            messagebox.showerror(title='Ошибка!', message='Количество точек должно быть не меньше двух!\n'
+                                                        '(чтобы как минимум можно было построить хотябы одну прямую)')
+            return
+        id_a, id_b, dots_l, dots_r = do_task(self.dots_dict, self.circle_info)
+        xa, ya = self.dots_dict[id_a]
+        xb, yb = self.dots_dict[id_b]
+        line = self.canv.create_line(xa * k_scale + x_axis_center, (-ya) * k_scale + y_axis_center, xb * k_scale + x_axis_center, (-yb) * k_scale + y_axis_center)
+        self.frame_res = Frame(self.root, bg='#9c79d1', highlightthickness=0)
+        self.frame_res.place(relwidth=0.3, relheight=0.68, rely=0.32)
+
+        self.lab_dots_left = Label(self.frame_res, font='"monotxt_iv50" 10', bg='#9c79d1', fg='#47385e', justify=CENTER, text='---------------------ТОЧКИ-СЛЕВА---------------------')
+        self.lab_dots_left.place(relwidth=1, rely=0)
+        self.listbox_dots_left = Listbox(self.frame_res)
+        for i in dots_l:
+            string = 'id={}    x={} y={}'.format(i[0], i[1], i[2])
+            self.listbox_dots_left.insert(END, string)
+        self.listbox_dots_left.place(relwidth=0.9, relx=0.05, relheight=0.3, rely=0.04)
+
+        self.lab_dots_right = Label(self.frame_res, font='"monotxt_iv50" 10', bg='#9c79d1', fg='#47385e', justify=CENTER, text='---------------------ТОЧКИ-СПРАВА---------------------')
+        self.lab_dots_right.place(relwidth=1, rely=0.36)
+        self.listbox_dots_right = Listbox(self.frame_res)
+        for i in dots_r:
+            string = 'id={}    x={} y={}'.format(i[0], i[1], i[2])
+            self.listbox_dots_right.insert(END, string)
+        self.listbox_dots_right.place(relwidth=0.9, relx=0.05, relheight=0.3, rely=0.4)
+
+        # self.frame_res.place_forget()
 
 if __name__ == '__main__':
     win = App()

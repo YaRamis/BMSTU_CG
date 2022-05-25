dc9ab1320a52f8419139f69f55db7ce6c882f519
Myrzabekov Ramis <myyrzabekovrm@student.bmstu.ru>
2022-05-25 05:45:44 +0300

    algorythm realized

--- ЛР 09/app.py
+++ ЛР 09/app.py
@@ -3,6 +3,9 @@ from tkinter import messagebox
 from tkinter import ttk
 from tkinter import colorchooser
 from math import sqrt
+from numpy import sign
+from numpy import matrix
+import numpy as np
 
 polygonColor = '#00ff00'
 cutterColor = '#000000'
@@ -40,33 +43,43 @@ def check_cutter(cutter):
 
     return True
 
+def visibility(point, begin, end):
+    tmp1 = (point[0] - begin[0]) * (end[1] - begin[1])
+    tmp2 = (point[1] - begin[1]) * (end[0] - begin[0])
+    res = tmp1 - tmp2
+    
+    if -1e-7 < res < 1e-7:
+        res = 0
+    return sign(res)
 
-def get_scalar_mul(fvector, svector):
-    return fvector[0] * svector[0] + fvector[1] * svector[1]
+def check_lines_crossing(begin1, end1, begin2, end2):
+    vis1 = visibility(begin1, begin2, end2)
+    vis2 = visibility(end1, begin2, end2)
 
-def get_normal(dot_start, dot_end, dot_check):
-    vect = get_vect(dot_start, dot_end)
-    normal = None
-
-    if vect[0] == 0:
-        normal = [1, 0]
+    if (vis1 < 0 and vis2 > 0) or (vis1 > 0 and vis2 < 0):
+        return True
     else:
-        normal = [-vect[1] / vect[0], 1]
+        return False
 
-    if get_scalar_mul(get_vect(dot_end, dot_check), normal) < 0:
-        for i in range(len(normal)):
-            normal[i] = -normal[i]
+def get_cross_point(begin1, end1, begin2, end2):
+    coef = []
+    coef.append([end1[0] - begin1[0], begin2[0] - end2[0]])
+    coef.append([end1[1] - begin1[1], begin2[1] - end2[1]])
 
-    return normal
+    rights = []
+    rights.append([begin2[0] - begin1[0]])
+    rights.append([begin2[1] - begin1[1]])
 
-def get_normals(cut):
-    normals = []
-    cutlen = len(cut)
+    coef_tmp = matrix(coef)
+    coef_tmp = coef_tmp.I
+    coef = [[coef_tmp.item(0), coef_tmp.item(1)], [coef_tmp.item(2), coef_tmp.item(3)]]
 
-    for i in range(cutlen):
-        normals.append(get_normal(cut[i], cut[(i + 1) % cutlen], cut[(i + 2) % cutlen]))
+    coef_tmp = matrix(coef)
+    param = coef_tmp.__mul__(rights)
+    
+    x, y = begin1[0] + (end1[0] - begin1[0]) * param.item(0), begin1[1] + (end1[1] - begin1[1]) * param.item(0)
 
-    return normals
+    return [x, y]
 
 class App():
     def __init__(self) -> None:
@@ -153,7 +166,7 @@ class App():
         self.cutter_edges_id = []
         self.cutter_apexes = []
         self.cutter_edge = []
-        self.cutter_edges_amount = 0
+        self.cutter_apexes_amount = 0
 
         self.btn_addApex = ttk.Button(self.frame, text='Добавить вершину', style='default.TButton', command=self.add_apex)
         self.btn_addApex.place(relwidth=0.92, relx=0.04, rely=0.32, relheight=0.04)
@@ -183,7 +196,7 @@ class App():
         self.polygon_edges_id = []
         self.polygon_apexes = []
         self.polygon_edge = []
-        self.polygon_edges_amount = 0
+        self.polygon_apexes_amount = 0
 
         self.btn_addApexP = ttk.Button(self.frame, text='Добавить вершину', style='default.TButton', command=self.add_apexP)
         self.btn_addApexP.place(relwidth=0.92, relx=0.04, rely=0.52, relheight=0.04)
@@ -202,6 +215,8 @@ class App():
 
         self.btn_cut = ttk.Button(self.frame, text='Выполнить отсечение', style='default.TButton', command=self.cut)
         self.btn_cut.place(relwidth=0.92, relx=0.04, rely=0.74, relheight=0.04)
+
+        self.res_edges_id = []
 
         # UNFOCUS
         self.frame.bind_all('<Button-1>', self.unfocus)
@@ -252,13 +267,15 @@ class App():
         self.polygon_edges_id = []
         self.polygon_apexes = []
         self.polygon_edge = []
-        self.polygon_edges_amount = 0
+        self.polygon_apexes_amount = 0
         
         self.cutter_apexes_tmp = []
         self.cutter_edges_id = []
         self.cutter_apexes = []
         self.cutter_edge = []
-        self.cutter_edges_amount = 0
+        self.cutter_apexes_amount = 0
+
+        self.res_edges_id = []
 
         self.last_action = []
     
@@ -266,15 +283,18 @@ class App():
         if self.polygon_apexes != []:
             for id in self.polygon_edges_id:
                 self.canv.delete(id)
+            for id in self.res_edges_id:
+                self.canv.delete(id)
+            self.res_edges_id = []
             self.polygon_apexes_tmp = []
             self.polygon_edges_id = []
             self.polygon_apexes = []
             self.polygon_edge = []
-            self.polygon_edges_amount = 0
+            self.polygon_apexes_amount = 0
         self.polygon_edge.append((event.x, event.y))
         self.polygon_apexes_tmp.append((event.x, event.y))
         if len(self.polygon_edge) == 2:
-            self.polygon_edges_amount += 1
+            self.polygon_apexes_amount += 1
             xb, yb = self.polygon_edge[0]
             xe, ye = self.polygon_edge[1]
             self.polygon_edges_id.append(self.canv.create_line(xb, yb, xe, ye, fill=polygonColor))
@@ -285,15 +305,18 @@ class App():
         if self.cutter_apexes != []:
             for id in self.cutter_edges_id:
                 self.canv.delete(id)
+            for id in self.res_edges_id:
+                self.canv.delete(id)
+            self.res_edges_id = []
             self.cutter_apexes_tmp = []
             self.cutter_edges_id = []
             self.cutter_apexes = []
             self.cutter_edge = []
-            self.cutter_edges_amount = 0
+            self.cutter_apexes_amount = 0
         self.cutter_edge.append((event.x, event.y))
         self.cutter_apexes_tmp.append((event.x, event.y))
         if len(self.cutter_edge) == 2:
-            self.cutter_edges_amount += 1
+            self.cutter_apexes_amount += 1
             xb, yb = self.cutter_edge[0]
             xe, ye = self.cutter_edge[1]
             self.cutter_edges_id.append(self.canv.create_line(xb, yb, xe, ye, fill=cutterColor))
@@ -320,15 +343,18 @@ class App():
         if self.cutter_apexes != []:
             for id in self.cutter_edges_id:
                 self.canv.delete(id)
+            for id in self.res_edges_id:
+                self.canv.delete(id)
+            self.res_edges_id = []
             self.cutter_apexes_tmp = []
             self.cutter_edges_id = []
             self.cutter_apexes = []
             self.cutter_edge = []
-            self.cutter_edges_amount = 0
+            self.cutter_apexes_amount = 0
         self.cutter_edge.append((x, y))
         self.cutter_apexes_tmp.append((x, y))
         if len(self.cutter_edge) == 2:
-            self.cutter_edges_amount += 1
+            self.cutter_apexes_amount += 1
             xb, yb = self.cutter_edge[0]
             xe, ye = self.cutter_edge[1]
             self.cutter_edges_id.append(self.canv.create_line(xb, yb, xe, ye, fill=cutterColor))
@@ -355,15 +381,18 @@ class App():
         if self.polygon_apexes != []:
             for id in self.polygon_edges_id:
                 self.canv.delete(id)
+            for id in self.res_edges_id:
+                self.canv.delete(id)
+            self.res_edges_id = []
             self.polygon_apexes_tmp = []
             self.polygon_edges_id = []
             self.polygon_apexes = []
             self.polygon_edge = []
-            self.polygon_edges_amount = 0
+            self.polygon_apexes_amount = 0
         self.polygon_edge.append((x, y))
         self.polygon_apexes_tmp.append((x, y))
         if len(self.polygon_edge) == 2:
-            self.polygon_edges_amount += 1
+            self.polygon_apexes_amount += 1
             xb, yb = self.polygon_edge[0]
             xe, ye = self.polygon_edge[1]
             self.polygon_edges_id.append(self.canv.create_line(xb, yb, xe, ye, fill=polygonColor))
@@ -387,9 +416,21 @@ class App():
                 self.canv.delete(id)
             return
         
+        vect1 = get_vect(self.cutter_apexes_tmp[0], self.cutter_apexes_tmp[1])
+        vect2 = get_vect(self.cutter_apexes_tmp[1], self.cutter_apexes_tmp[2])
+
+        sign = None
+        if get_vect_mul(vect1, vect2) > 0:
+            sign = 1
+        else:
+            sign = -1
+        if sign < 0:
+            self.cutter_apexes_tmp.reverse()
+
         self.cutter_apexes = self.cutter_apexes_tmp
         self.cutter_edges_id.append(self.canv.create_line(self.cutter_apexes[0][0], self.cutter_apexes[0][1], self.cutter_apexes[-1][0], self.cutter_apexes[-1][1], fill=cutterColor))
         self.cutter_apexes.append(self.cutter_apexes_tmp[0])
+        self.cutter_apexes_amount += 1
         self.cutter_apexes_tmp = []
         self.cutter_edge = []
     
@@ -397,54 +438,79 @@ class App():
         if len(self.polygon_apexes_tmp) < 3:
             messagebox.showerror(title='Ошибка!', message='У многоугольника количество вершин должно быть больше двух!')
             return
+        vect1 = get_vect(self.polygon_apexes_tmp[0], self.polygon_apexes_tmp[1])
+        vect2 = get_vect(self.polygon_apexes_tmp[1], self.polygon_apexes_tmp[2])
 
+        sign = None
+        if get_vect_mul(vect1, vect2) > 0:
+            sign = 1
+        else:
+            sign = -1
+        if sign < 0:
+            self.polygon_apexes_tmp.reverse()
         self.polygon_apexes = self.polygon_apexes_tmp
         self.polygon_edges_id.append(self.canv.create_line(self.polygon_apexes[0][0], self.polygon_apexes[0][1], self.polygon_apexes[-1][0], self.polygon_apexes[-1][1], fill=polygonColor))
         self.polygon_apexes_tmp = []
         self.polygon_edge = []
 
     def cut(self):
-        normals = get_normals(self.cutter_coords)
-        for line in self.all_lines:
-            self.cyrusbeck(line, normals)
+        if self.cutter_apexes == [] or self.polygon_apexes == []:
+            messagebox.showerror(title='Ошибка!', message='Проверьте замкнутость отсекателя и многоугольника!')
+            return
+
+        self.sutherland_hodgman()
     
-    def cyrusbeck(self, line, normals):
-        t_start = 0
-        t_end = 1
+    def sutherland_hodgman(self):
+        p = self.polygon_apexes
+        q = []
+        w = self.cutter_apexes
 
-        vect = get_vect(line[0], line[1])
-        cutlen = len(self.cutter_coords)
+        np = len(p)
+        nq = 0
+        nw = len(w)
 
-        for i in range(cutlen):
-            w_vect = get_vect(self.cutter_coords[(i + 1) % cutlen], line[0])
-            if self.cutter_coords[i] != line[0]:
-                w_vect = get_vect(self.cutter_coords[i], line[0])
-
-            vect_scal = get_scalar_mul(vect, normals[i])
-            w_vect_scal = get_scalar_mul(w_vect, normals[i])
-
-            if vect_scal == 0:
-                if w_vect_scal < 0:
-                    return
+        s = []
+        f = []
+        for i in range(nw - 1):
+            nq = 0
+            q = []
+            for j in range(np):
+                if j != 0:
+                    is_crossing = check_lines_crossing(s, p[j], w[i], w[i + 1])
+                    if is_crossing == True:
+                        q.append(get_cross_point(s, p[j], w[i], w[i + 1]))
+                        nq += 1
+                    else:
+                        if visibility(s, w[i], w[i + 1]) == 0:
+                            q.append(s)
+                            nq += 1
+                        elif visibility(p[j], w[i], w[i + 1]) == 0:
+                            q.append(s)
+                            nq += 1
+                else:
+                    f = p[j]
+                s = p[j]
+                if visibility(s, w[i], w[i + 1]) > 0:
+                    continue
+                q.append(s)
+                nq += 1
+            if nq == 0:
                 continue
-
-            t = -w_vect_scal / vect_scal
-            if vect_scal > 0:
-                if t > t_start:
-                    t_start = t
-            else:
-                if t < t_end:
-                    t_end = t
-
-            if t_start > t_end:
-                break
-
-        if t_start < t_end:
-            dot_start = [round(line[0][0] + vect[0] * t_start),
-                        round(line[0][1] + vect[1] * t_start)]
-            dot_end = [round(line[0][0] + vect[0] * t_end),
-                    round(line[0][1] + vect[1] * t_end)]
-            self.canv.create_line(dot_start[0], dot_start[1], dot_end[0], dot_end[1], fill=resultColor)
+            is_crossing = check_lines_crossing(s, f, w[i], w[i + 1])
+            if is_crossing == False:
+                p = q
+                np = nq
+                continue
+            q.append(get_cross_point(s, f, w[i], w[i + 1]))
+            nq += 1
+            p = q
+            np = nq
+        
+        self.draw_result(p, np)
+    
+    def draw_result(self, p, np):
+        for i in range(np):
+            self.res_edges_id.append(self.canv.create_line(p[i - 1][0], p[i - 1][1], p[i][0], p[i][1], fill=resultColor))
 
     def entry_mode_x(self, event):
         if (self.ent_xlt.get() == 'X'):
@@ -493,18 +559,11 @@ class App():
         self.canv.config(width=w, height=h)
     
     def redo(self, event):
-        for i in range(len(self.last_action)):
-            action = self.last_action[i][1]
-            if action == 'Create circle':
-                for id in self.last_action[i][0]:
-                    self.canv.delete(id)
-            if action == 'Create ellipse':
-                for id in self.last_action[i][0]:
-                    self.canv.delete(id)
+        # for i in range(len(self.last_action)):
         self.last_action = []
     
     def prog_info(self):
-        messagebox.showinfo(title='О программе', message='Условие:\РЕАЛИЗАЦИЯ И ИЗУЧЕНИЕ АЛГОРИТМОВ ОТСЕЧЕНИЯ Кируса-Бека\n'
+        messagebox.showinfo(title='О программе', message='Условие:\РЕАЛИЗАЦИЯ И ИЗУЧЕНИЕ АЛГОРИТМОВ ОТСЕЧЕНИЕ ВЫПУКЛЫМ ОТСЕКАТЕЛЕМ\n'
                                                         '-----------------------------------------------------------\n')
         
     def author_info(self):
